6c70d0912c6b35f51537c4650bf7b61de305cbbe
Ruchithaakula <67132687+Ruchithaakula@users.noreply.github.com>
2020-06-21 20:16:13 +0530

    Add files via upload

---
+++ Assignment3_equal or not.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ")
+# if a is b: - Compares id's, and can cause inconsistencies. Use == instead.
+if a == b:
+  print("Both inputs are equal")
+else:
+  print("Your input is not equal.")
+
+
+# In[ ]:
+
+
+
+

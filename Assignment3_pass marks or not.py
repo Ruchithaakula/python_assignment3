67aa6cda13f2255b7e765a20092a1945537edf7e
Ruchithaakula <67132687+Ruchithaakula@users.noreply.github.com>
2020-06-21 20:17:01 +0530

    Add files via upload

---
+++ Assignment3_pass marks or not.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a = int(input()) #taking input from user
+if a>35
+	print(" greater than pass marks ")
+else:
+	print( " less than pass marks ")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

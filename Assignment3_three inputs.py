f471544405865aabeb91332c91b864408b3a2eff
Ruchithaakula <67132687+Ruchithaakula@users.noreply.github.com>
2020-06-22 09:58:52 +0530

    Add files via upload

---
+++ Assignment3_three inputs.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+print("enter a value")
+a=input()
+a=int(a)
+print("enter b value")
+b=input()
+b=int(b)
+print("enter c value")
+c=input()
+c=int(c)
+if a==b and b==c:
+    print("all are equal")
+elif a==b or b==c or c==a:
+    print(" any two are equal")
+else:
+    print(" any condition is not satisfied")
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

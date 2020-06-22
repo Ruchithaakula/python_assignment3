3bec966713036e1c3096b4f5aa20245daa33178b
Ruchithaakula <67132687+Ruchithaakula@users.noreply.github.com>
2020-06-22 09:32:29 +0530

    Add files via upload

---
+++ Assignment3_sum greater or less than.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+print ("Enter first number")
+first = input()
+first=int(first)
+print("Enter second number")
+second = input()
+second=int(second)
+sum = first+second
+if sum>5:
+    print ("Sum is greater than 5:",sum)
+elif sum==5:
+    print ("Sum is equal to 5:",sum)
+else:
+    print ("Sum is lesser than 5:",sum)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
